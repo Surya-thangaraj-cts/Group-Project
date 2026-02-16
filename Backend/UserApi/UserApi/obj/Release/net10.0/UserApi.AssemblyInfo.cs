5898801c88c7d661b3978135c600f475478a10f4
Thangaraj <2464489@cognizant.com>
2026-02-16 09:17:59 +0530

    changes

--- Backend/UserApi/UserApi/obj/Release/net10.0/UserApi.AssemblyInfo.cs
+++ Backend/UserApi/UserApi/obj/Release/net10.0/UserApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7247ad21002199d1d4c9c46c3dddb52b32919ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
